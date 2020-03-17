e9b8922a00d22ba0e91679018ded9a780462c1aa
kesavamuthu <kesavamuthu77@gmail.com>
2020-03-17 20:50:49 +0530

    largest number finder based on the second largest

---
+++ src/com/fun/order/LargeNumberIndexFinder.java
@@ -0,0 +1,23 @@
+package com.fun.order;
+
+public class LargeNumberIndexFinder
+{
+
+    public static void main(String[] args)
+    {
+        int[] a = {6, 3, 2, 1};
+        int largest = a[0];
+        int secLarge = 0;
+        int index = 0;
+        for(int i = 1; i < a.length; ++i){
+            if(a[i] > largest){
+                secLarge = largest;
+                largest = a[i];
+                index = i;
+            }
+            else if(secLarge < a[i])
+                secLarge = a[i];
+        }
+        System.out.println(largest >= secLarge * 2 ? index : -1);
+    }
+}
