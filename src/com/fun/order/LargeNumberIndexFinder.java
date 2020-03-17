71ad955afc958e3391d1e67198013706237e8c34
kesavamuthu <kesavamuthu77@gmail.com>
2020-03-17 23:51:09 +0530

    fixed single element array test case.

--- src/com/fun/order/LargeNumberIndexFinder.java
+++ src/com/fun/order/LargeNumberIndexFinder.java
@@ -5,10 +5,10 @@ public class LargeNumberIndexFinder
 
     public static void main(String[] args)
     {
-        int[] a = {6, 3, 2, 1};
+        int[] a = {1};
         int largest = a[0];
         int secLarge = 0;
-        int index = 0;
+        int index = -1;
         for(int i = 1; i < a.length; ++i){
             if(a[i] > largest){
                 secLarge = largest;
@@ -18,6 +18,7 @@ public class LargeNumberIndexFinder
             else if(secLarge < a[i])
                 secLarge = a[i];
         }
-        System.out.println(largest >= secLarge * 2 ? index : -1);
+        System.out.println(largest >= secLarge * 2 && index != -1 ? index : -1);
     }
+
 }
